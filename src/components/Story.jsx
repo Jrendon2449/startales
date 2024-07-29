fa0960fad38a8668b43e64cbab1edc968c6cbf61
William <williamli1@berkeley.edu>
2024-07-29 11:32:31 -0700

    formatting

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Nav from './Nav';
 import SearchButton from './SearchButton';
+import Storyboard from './Storyboard';
 
 function Story() {
   const navigate = useNavigate();
@@ -13,6 +14,7 @@ function Story() {
   return (
     <div className="page story">
       <Nav title="Story" navigate={navigate} return_path={"/"}/>
+      <Storyboard />
       <SearchButton onClick={handleSearchClick} />
     </div>
   );
---
+++ src/components/Storyboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Storyboard.css';
+
+export default function Storyboard() {
+    return (
+        <div className="storyboard">
+            <h1> The story </h1>
+        </div>
+    )
+}

8331850f40597f8ec9150370fb6fc12613fb4664
40224617 <zx96748520@gmail.com>
2024-08-05 23:19:00 -0700

    added information page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,36 @@
 import React from "react";
 import Button from "./Button";
 import { Link } from "react-router-dom";
+import { IoMdInformationCircleOutline } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+
 export default function Home(props) {
-  
   //localStorage.clear();
+  const navigate = useNavigate();
+  const handleIconClick = () => {
+    navigate("/information");
+  };
   return (
     <>
       <h1 className="home--title">Star Tales</h1>
       <div className="buttons">
-        <h2><em>Today let's...</em></h2>
+        <h2>
+          <em>Today let's...</em>
+        </h2>
         <Link to="/story">
-          <Button name="Write a Story" onClick={props.fullscreen}/>
+          <Button name="Write a Story" onClick={props.fullscreen} />
         </Link>
         <Link to="/science_overview">
-          <Button name="Learn Science" onClick={props.fullscreen}/>
+          <Button name="Learn Science" onClick={props.fullscreen} />
         </Link>
         <Link to="/archive">
-          <Button name="Archive" onClick={props.fullscreen}/>
+          <Button name="Archive" onClick={props.fullscreen} />
         </Link>
-        {/** 
-        <Link to="/information">
-          <Button name="Information" onClick={props.fullscreen}/>
-        </Link>
-        */}
+        <IoMdInformationCircleOutline
+          size="50px"
+          onClick={handleIconClick}
+          className="informationIcon"
+        />
       </div>
     </>
   );
--- src/components/information/information.jsx
+++ src/components/information/information.jsx
@@ -1,33 +1,46 @@
-import React, { useState } from 'react';
-import Button from '../Button';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Nav from "../Nav";
-
+import "../css/information.css";
 
 const Information = () => {
-    return (
-        <>
-        <Nav title="Story" navigate={navigate} return_path={"/"} />
-        <p className = "appIntroduction">  
-        This app is designed for begineer who wants to have more interactive experience when learning more about the stars in the sky while stargazing
-        , and also for intermidiate stargazers who want to learn more about the scientific facts of the stars. 
-        </p>
-        <p className = "storyIntroduction">
-        The sotry pages allow you to create your own story based on your personal preference includes
-        genre, mood, and language. We also provide different length and reading level for different users 
-        based on their age. We believe that the process of creating the story can give you a better learning
-        experience and motivation while playing with the setting. We believe this app can help you to learn
-        more about the stars youu like in a ,ore intersting and memorizable way. 
-        </p>
-        <p className = "scienceIntroduction">
-        The science pages are designed for users that are more experienced with stargazing but also want to learn 
-        more about the scientific facts about the stars. This app will give users access to the information 
-        right away with a few clicks, save the users' time to go through the wikipedia and some other websites
-        for extra scitific information. Which also gives the users who want to know more about the stars after generating a story of the star.
-        </p>
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Nav title="Information" navigate={navigate} return_path={"/"} />
+      <div className="information-container">
+        <div className="information-wrapper">
+          <b>Introduction to the app: </b> <br />
+          <p className="appIntroduction">
+            This app is designed for begineer who wants to have more interactive
+            experience when learning more about the stars in the sky while
+            stargazing , and also for intermidiate stargazers who want to learn
+            more about the scientific facts of the stars.
+          </p>
+          <br />
+          <b>Introduction to Story Pages: </b> <br />
+          <p className="storyIntroduction">
+            The sotry pages allow you to create your own story of a star based
+            on your personal preference includes genre, mood, and language. We
+            also provide different length and reading level for different users
+            based on their age. We believe that the process of creating the
+            story can give the users a better learning experience and motivation
+            while playing with the configuration of the story
+          </p>
+          <br />
+          <b>Introduction to Science Pages: </b> <br />
+          <p className="scienceIntroduction">
+            The science pages are designed for users that are more experienced
+            with stargazing those want to learn more about the scientific facts
+            about the stars. This app will give users access to the information
+            right away with a few clicks, save the users' time to go through the
+            wikipedia and some other websites for extra scitific information.
+          </p>
+        </div>
+      </div>
     </>
-    );
+  );
 };
 
-export default Information
-
+export default Information;

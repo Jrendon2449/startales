c4f7b5b0e3297bee31d8d491288d631d654be38e
40224617 <zx96748520@gmail.com>
2024-08-07 00:54:13 -0700

    fixed the layout of the search button

--- src/components/search/SearchButton.jsx
+++ src/components/search/SearchButton.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import MagnifyingGlass from '../../assets/magnifying_glass.svg';
-import '../css/SearchButton.css';
+import React from "react";
+import MagnifyingGlass from "../../assets/magnifying_glass.svg";
+import "../css/SearchButton.css";
 
 export default function SearchButton(props) {
-
-    return (
-        <>
-            <button className="search--button" onClick={props.onClick}>
-                <img src={MagnifyingGlass} alt="Search" className="search--icon"/>
-            </button>
-        </>
-    )
+  return (
+    <button className="search--button" onClick={props.onClick}>
+      <h3>Search For</h3>
+      <img src={MagnifyingGlass} alt="Search" className="search--icon" />
+      <h3>More Stars!</h3>
+    </button>
+  );
 }
